e8d728dbd97185b7dac13911636178c38603526a
tcpack456 <70459271+tcpack456@users.noreply.github.com>
2020-08-30 12:44:44 -0400

    Update random.php

--- random.php
+++ random.php
@@ -1,15 +1,14 @@
 <?php
-    $files = array(6);
+    $files = array();
 
-    $files[1] = fopen('lion.txt', 'r');
-    $files[2] = fopen('tiger.txt', 'r');
-    $files[3] = fopen('wolf.txt', 'r');
-    $files[4] = fopen('bear.txt', 'r');
-    $files[5] = fopen('webelos.txt', 'r');
-    $files[6] = fopen('arrow-of-light.txt', 'r');
-  
+    $files[] = fopen('./data/lion.txt', 'r');
+    $files[] = fopen('./data/tiger.txt', 'r');
+    $files[] = fopen('./data/wolf.txt', 'r');
+    $files[] = fopen('./data/bear.txt', 'r');
+    $files[] = fopen('./data/webelos.txt', 'r');
+    $files[] = fopen('./data/arrow-of-light.txt', 'r');
 
-    $scouts = array(11);
+    $scouts = array();
 
     for($i=0 ; $i<count($files); $i++)
     {
@@ -18,15 +17,15 @@
         {
             //$url = "https://www.trails-end.com/store/scout/" . trim($data[1]);
             //echo "<li><a href=\"$url\">" . trim($data[0]) . "</a></li>\n";
-            $scouts[11] = $data;
+            $scouts[] = $data;
         }
     }
 
-    $n = rand(11, count($scouts));
+    $n = rand(0, count($scouts));
     
     $scout = $scouts[$n];
-    $name = Alex B.($scout[0]);
-    $code = V9WS0069($scout[1]);
+    $name = trim($scout[0]);
+    $code = trim($scout[1]);
 
     $url = "https://www.trails-end.com/store/scout/" . $code;
     header('Location: ' . $url);
