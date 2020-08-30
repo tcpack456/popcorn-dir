a4e9d7c69b57d74b10d2a24e29c37555bfd5af9b
tcpack456 <70459271+tcpack456@users.noreply.github.com>
2020-08-30 15:57:31 -0400

    Create randmoizer.php

---
+++ randmoizer.php
@@ -0,0 +1,21 @@
+<script type=”text/javascript”>
+    var urls = [
+        “https://www.trails-end.com/store/scout/2M1DD0CL”,
+        ‘https://www.trails-end.com/store/scout/V9WS0069’,
+        ‘https://www.trails-end.com/store/scout/OWH3GA77’,
+        ‘https://www.trails-end.com/store/scout/W5L8NJG7’,
+        ‘https://www.trails-end.com/store/scout/K8JCOSXL’,
+        ‘https://www.trails-end.com/store/scout/INW8IY69‘,
+        ‘https://www.trails-end.com/store/scout/RP9YPK2I’,
+        ‘https://www.trails-end.com/store/scout/6EYMG5UT’,
+        ‘https://www.trails-end.com/store/scout/01AJ8MCH’,
+        ‘https://www.trails-end.com/store/scout/TCKGBZ0O’,
+        ‘https://www.trails-end.com/store/scout/8OPYINRX’,
+       
+    ];
+    function goSomewhere() {
+        var url = urls[Math.floor(Math.random()*urls.length)];
+        window.open(url); // redirect
+    }
+</script>
+<button onclick=”goSomewhere() “>Support a Random Scout!</button>
